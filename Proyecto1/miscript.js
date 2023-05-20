f53e1c8a114bb734b610a90680718b50037b37c3
meyling83 <116660994+meyling83@users.noreply.github.com>
2023-05-20 13:37:15 +0200

    Add files via upload

--- Proyecto1/miscript.js
+++ Proyecto1/miscript.js
@@ -1,16 +1,39 @@
 let cuentaAtras=document.getElementById("cuentaAtras");
+var contador=30;
 function comenzarCuentaAtras(){
-    setInterval(hora,1000);
+    
+    setInterval(segundo,1000);
+    //si no he dado clic en finalizado********como lo hago
+    
 }
 
-function hora(){
-    //obtengo la fecha actual, y de la fecha actual obtengo las horas, minutos y segundos
-    let t=new Date();
-    //el padStart es para que si tiene un solo lugar salga un 0 delante y lo de con 2 digitos, tengo que convertirlo antes a string
-    let h=String(t.getHours()).padStart(2,"0");
-    let m=String(t.getMinutes()).padStart(2,"0");
-    let s=String(t.getSeconds()).padStart(2,"0");
-    //concatenamos lo anterior
-    let textoHora=h+ ":" + m + ":" + s;
-    texto.textContent=textoHora;
+function segundo(){
+    contador--;
+   let texto=document.getElementById("cuentaAtras");
+   texto.textContent=contador;
+   if(contador==0)
+        setTimeout(tiempoFinalizado,30000);
+}
+function tiempoFinalizado(){
+    clearInterval();
+    //cuentaAtras=0;
+    let texto=document.getElementById("contador");
+    texto.textContent=cuentaAtras;
+    let sonido=document.getElementById("audioFinal");
+    sonido.play();
+    alert("Game Over: Se acabó el tiempo. Vuelve a intentarlo");
+}
+function finalizado(){
+    clearTimeout();
+    clearInterval();
+    let fecha=new Date().toDateString();
+    let respuesta1=getElementById(respuesta1).value;
+    let respuesta2=getElementById(respuesta2).value;
+    let respuesta3=getElementById(respuesta3).value;
+    let respuesta4=getElementById(respuesta4).value;
+    let respuesta5=getElementById(respuesta5).value;
+    alert (fecha + respuesta1 + respuesta2 +respuesta3 +respuesta4+respuesta5);
+}
+function volverAJugar(){
+    location.reload();
 }
