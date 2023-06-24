42256c28978cd429ba428cfbf5a7c566a13cd9df
meyling83 <116660994+meyling83@users.noreply.github.com>
2023-06-24 10:02:49 +0200

    Update misScripts.js

--- Proyecto_Buscador/misScripts.js
+++ Proyecto_Buscador/misScripts.js
@@ -1,37 +1,63 @@
-let opcion=document.getElementById("miSelector").value;
-document.getElementById("miSelector").addEventListener("change",()=>
-    opcion=document.getElementById("miSelector").value
-);
-document.getElementById("miInput").addEventListener("keydown",validar);
-let miInput=document.getElementById("miInput").value;
-let miListado=document.getElementById("miListado");
-document.getElementById("miBoton").addEventListener("click",buscar);
+//variables
+let selector=document.getElementById("miSelector");
+let input=document.getElementById("miInput");
+let boton=document.getElementById("miBoton");
+let listar=document.getElementById("miListado");
+
+let archivo='peliculas.json';
+
+//escuchadores de eventos
+selector.addEventListener("change",cambiarArchivo);
+input.addEventListener("keydown",verificarInput);
+boton.addEventListener("click",buscar);
+
+function cambiarArchivo(){
+    archivo=selector.value;
+}
+
+function verificarInput(e){
+    let valor=e.target.value;
+    
+    if((e.keyCode<65 || e.keyCode>90) && e.keyCode!=32 && e.keyCode!=8 ){
+        e.preventDefault();
+    }
+}
+
 function buscar(){
-    var url="http://localhost:3000/"+opcion;
-    fetch(url)
+    listar.innerHTML="";
+    fetch(archivo)
     .then(response=>response.json())
     .then(datos=>{
-        for (elemento of datos){
-            if(elemento.nombre.startsWith(miInput))
-            {
-                let parrafo= document.createElement("p");
-                parrafo.textContent=elemento.sinopsis;
+        salida=datos.data;
+        for (let item of salida){
+            if(item.nombre.startsWith(input.value.toUpperCase()))
+            { 
+                //creo un parrafo con el nombre de la pelicula
+                //añado la sinopsis, oculto
+                let p= document.createElement("p");
+                p.id=item.nombre;
+                p.innerHTML=item.sinopsis;
+                p.style.display="none";
+
+                //añado el elemento lista y sus eventos
                 let li=document.createElement("li");
-                li.textContent=elemento.nombre;
-                li.appendChild(parrafo);
-                miListado.appendChild(li);
-                li.addEventListener("mouseover",()=>parrafo.style.display="block");
-                li.addEventListener("mouseout",()=>parrafo.style.display="none");
+                li.innerHTML=item.nombre;
+                
+                //eventos de cada elemento de la lista
+                li.addEventListener("mouseover",function(){
+                    let p=document.getElementById(item.nombre);
+                    p.style.display="block"
+                });
+                li.addEventListener("mouseout",function(){
+                    let p=document.getElementById(item.nombre);
+                    p.style.display="none"
+                });
+
+                li.appendChild(p);
+                listar.appendChild(li);
             }
         }
     })
     .catch(error=>console.log(error));
 
-}
-function validar(e){
-    let valor=e.target.value;
-    
-    if(e.keyCode<65 || e.keyCode>90 ){
-        e.preventDefault();
-    }
 }
